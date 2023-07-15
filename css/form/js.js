4ccb4342890feddf31dc8b6edb4837af620ed24f
Павел Струначев <pavelstrunacev@iMac-Pavel.local>
2023-07-15 21:37:02 +0500

    Накинуд JS - не работает - переделать

---
+++ css/form/js.js
@@ -0,0 +1,13 @@
+const openPopUp = document.querySelector('btn--login');
+const closePopUp = document.querySelector('btn--login');
+const PopUp = document.querySelector('wrapper');
+
+
+/*
+openPopUp.addEventListener('click', function(e){
+  e.preventDefault();
+  PopUp.classList.add ('active');
+})
+closePopUp.addEventListener('click', () => {
+  PopUp.classList.remove('active')
+})
