3915d0fd41e33466a79850346b2e5fc45deda3d0
Павел Струначев <pavelstrunacev@iMac-Pavel.local>
2023-07-17 15:39:13 +0500

    управление элементами в html

---
+++ js/lesson6/ls6.js
@@ -0,0 +1,25 @@
+/*
+//функции
+
+function info(hello) {// функция (в скобки вводим параметры)
+  console.log (hello + 'ку');
+}
+
+info("word"); // вызов функции
+
+
+function summa(arr) { //функция с параметром
+  var sum = 0; // переменная со значением 0
+  for (var i = 0; i < arr.length; i++) // цикл фор от нуля до кол-ва элементов в массиве +1
+  sum += arr[i]
+  return sum; //возвращаем полученное личсло в любую нужную еперемнную для дальнейшей работы
+}
+
+var array = [5, 8, 1];
+var array_1 = [2, 81, 1];
+var array_2 = [45, 8, 1];
+var res = summa (array)
+console.log (res);
+//summa (array_1)
+//summa (array_2)
+*/
---
+++ js/lesson7sob/ls7.js
@@ -0,0 +1,44 @@
+//мой вариант замены классов
+/*
+document.querySelector('.btn').addEventListener('click', function(){
+  document.querySelector('.btn').classList.add ('open');
+})
+document.querySelector('.btn2').addEventListener('click', function(){
+  document.querySelector('.btn').classList.remove ('open');
+})
+
+document.querySelector('.btn2').addEventListener('click', function(){
+  document.querySelector('.btn2').classList.add ('open');
+})
+document.querySelector('.btn').addEventListener('click', function(){
+  document.querySelector('.btn2').classList.remove ('open');
+})
+*/
+
+/* считаем клики
+var click = 0;
+function onClickbutton() {
+  click++;
+  console.log(click)
+}
+*/
+
+/*ВЫВОД ДАННЫХ НА ЭКРАН
+var click = 0;
+function onClickbutton(el) {
+  click++;
+  el.innerHTML = "Нажатий на кнопку: "+click;
+  console.log (el.name); // вывод значения с атрибута
+}
+*/
+
+//ПОЛУЧЕНИЕ ДАННЫХ С ЭКРАНА
+function onClickbutton(el){
+  if (el.value == "hello") {
+  alert ("Здорова");
+  }
+  console.log (el.value);
+}
+
+//el.style.background = "red" - обращение к css свойству el.style
+//el.style.cssText = "Border-radius: 5px; background-color: red" - обращение к css для работы как в файле
