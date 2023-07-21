fbd2711dcd72f633b34fa957b6bfb01ddbc38486
pavelgletcher <pavelgletcher@yandex.ru>
2023-07-21 23:32:14 +0500

    my burger ok

--- css/burgermenu/js.js
+++ css/burgermenu/js.js
@@ -1,10 +1,8 @@
-function go() {
-    let box = document.querySelector ('.burger--box');
-    let line = document.querySelector ('.burger--line');
-    box.addEventListener ('click', function(){
-        line.classList.toggle('active');
+function burgermenu (){
+    let menu = document.querySelector ('.burger');
+    menu.addEventListener ('click', function(){
+        menu.classList.toggle ('active');
     });
-    
-}
+};
 
-go ();
+burgermenu ();
