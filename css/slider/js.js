23f745f6515f23c9e78a47745987d16f9b0fa64b
pavelgletcher <pavelgletcher@yandex.ru>
2023-07-24 00:19:17 +0500

    + собственный опыт с циклом, но еще не все

--- css/slider/js.js
+++ css/slider/js.js
@@ -1,13 +1,33 @@
 let sldstart = 0;
-let slditem = document.querySelector ('.slider--item');
+let slditem = document.querySelectorAll ('.slider--item');
+
+for (let i = 0; i < slditem.length; i++) {
+    document.querySelector ('.slider--btn--right').addEventListener ('click', function(){
+    sldstart = sldstart + 33.3333333
+    slditem[0].style.transform = 'translateX('+sldstart+'%)';
+    slditem[1].style.transform = 'translateX('+sldstart+'%)';
+    slditem[2].style.transform = 'translateX('+sldstart+'%)';
+});}
+
+for (let i = 0; i < slditem.length; i++) {
+    document.querySelector ('.slider--btn--left').addEventListener ('click', function(){
+    sldstart = sldstart - 33.3333333;
+    slditem[0].style.transform = 'translateX('+sldstart+'%)';
+    slditem[1].style.transform = 'translateX('+sldstart+'%)';
+    slditem[2].style.transform = 'translateX('+sldstart+'%)';
+});}
 
 
+
+
+/*
 document.querySelector ('.slider--btn--right').addEventListener ('click', function(){
     sldstart = sldstart + 100;
     if (sldstart >= 300) {
         sldstart = 0;
     }
     slditem.style.transform = 'translateX('+sldstart+'%)';
+    
 });
 
 document.querySelector ('.slider--btn--left').addEventListener ('click', function(){
@@ -17,3 +37,4 @@ document.querySelector ('.slider--btn--left').addEventListener ('click', functio
     }
     slditem.style.transform = 'translateX('+sldstart+'%)';
 });
+*/
