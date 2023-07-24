965d54c2eaffcf56380382068a301b4d1f1c30b2
Павел Струначев <pavelstrunacev@iMac-Pavel.local>
2023-07-24 18:02:44 +0500

    добавление нового слайдера с другой логикой

--- css/sliderfail/js.js
+++ css/sliderfail/js.js
@@ -1,3 +1,31 @@
+
+
+
+
+//ТРЕТЬЕ РЕШЕНИЕ НЕ ОТЛАЖЕНО
+/*
+let array = document.querySelectorAll('.slider--item');
+let go = 0;
+for (let i = 0; i < array.length; i++) {
+    const slditem = array[i];
+    let btntight = document.querySelector('.slider--btn--right');
+    btntight.addEventListener('click', function () {
+        go = go + 100;
+        slditem.style.transform = 'translateX(' + go + '%)';
+    });
+}
+for (let i = 0; i < array.length; i++) {
+    const slditem = array[i];
+    let btntight = document.querySelector('.slider--btn--left');
+    btntight.addEventListener('click', function () {
+        go = go - 100;
+        slditem.style.transform = 'translateX(' + go + '%)';
+    });
+};
+*/
+
+//ВТОРОЕ РЕШЕНИЕ НЕ ОТЛАЖЕНО
+/*
 let sldstart = 0;
 let slditem = document.querySelectorAll ('.slider--item');
 
@@ -16,11 +44,13 @@ for (let i = 0; i < slditem.length; i++) {
     slditem[1].style.transform = 'translateX('+sldstart+'%)';
     slditem[2].style.transform = 'translateX('+sldstart+'%)';
 });}
+*/
 
 
-
-
+//ПЕРВОЕ РЕШЕНИЕ НЕ ОТЛАЖЕНО
 /*
+let sldstart = 0;
+let slditem = document.querySelectorAll ('.slider--item');
 document.querySelector ('.slider--btn--right').addEventListener ('click', function(){
     sldstart = sldstart + 100;
     if (sldstart >= 300) {
---
+++ css/slidernew/js.js
@@ -0,0 +1,15 @@
+let line = document.querySelector ('.slider--line');
+let btnright = document.querySelector ('.slider--btn--right');
+let btnleft =  document.querySelector ('.slider--btn--left');
+let go = 0;
+
+btnright.addEventListener ('click', function (){
+    go = go + 25;
+    line.style.transform = 'translateX(' + -go + '%)';
+
+});
+
+btnleft.addEventListener ('click', function (){
+    go = go - 25;
+    line.style.transform = 'translateX(' + -go + '%)';
+});
