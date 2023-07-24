26e8b7c546b8cd3fc0e3fb1486625b741e693ea8
Павел Струначев <pavelstrunacev@iMac-Pavel.local>
2023-07-24 18:28:35 +0500

    добавление условий

--- css/slidernew/js.js
+++ css/slidernew/js.js
@@ -2,14 +2,22 @@ let line = document.querySelector ('.slider--line');
 let btnright = document.querySelector ('.slider--btn--right');
 let btnleft =  document.querySelector ('.slider--btn--left');
 let go = 0;
+let start = 0;
 
 btnright.addEventListener ('click', function (){
     go = go + 25;
     line.style.transform = 'translateX(' + -go + '%)';
-
+    console.log (go);
+    if (go > 50) {
+        go = -25;
+    }
 });
 
 btnleft.addEventListener ('click', function (){
     go = go - 25;
     line.style.transform = 'translateX(' + -go + '%)';
+    console.log (go);
+    if (go < 25) {
+        go = 100;
+    }
 });
