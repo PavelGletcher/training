2a60cb0ef5c17e1028e859d418238429ab0c604e
Павел Струначев <pavelstrunacev@iMac-Pavel.local>
2023-07-24 18:50:15 +0500

    добавление radio есть недочет

--- css/slidernew/js.js
+++ css/slidernew/js.js
@@ -1,6 +1,10 @@
 let line = document.querySelector ('.slider--line');
 let btnright = document.querySelector ('.slider--btn--right');
 let btnleft =  document.querySelector ('.slider--btn--left');
+let btndot = document.querySelector ('.slider--dot--item');
+let btndot2 = document.querySelector ('.slider--dot--item2');
+let btndot3 = document.querySelector ('.slider--dot--item3');
+let btndot4 = document.querySelector ('.slider--dot--item4');
 let go = 0;
 let start = 0;
 
@@ -19,5 +23,18 @@ btnleft.addEventListener ('click', function (){
     console.log (go);
     if (go < 25) {
         go = 100;
-    }
+    };
 });
+
+btndot.addEventListener ('click', function (){
+    line.style.transform = 'translateX(' + 0 + '%)';
+})
+btndot2.addEventListener ('click', function (){
+    line.style.transform = 'translateX(' + -25 + '%)';
+})
+btndot3.addEventListener ('click', function (){
+    line.style.transform = 'translateX(' + -50 + '%)';
+})
+btndot4.addEventListener ('click', function (){
+    line.style.transform = 'translateX(' + -75 + '%)';
+})
